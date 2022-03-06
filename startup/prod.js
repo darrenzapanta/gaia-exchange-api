7ac23c77d039b5e7748a776f724edafb30816936
Darren <darrenzapanta1@yahoo.com>
2022-03-05 20:14:35 -0800

    add cors to prod

--- startup/prod.js
+++ startup/prod.js
@@ -1,9 +1,16 @@
 const helmet = require("helmet");
 const compression = require("compression");
 const winston = require("winston");
+const cors = require("cors");
 
 module.exports = function (app) {
   winston.info("prod startup init..");
   app.use(helmet());
   app.use(compression());
+
+  app.use(
+    cors({
+      origin: [/.*./, "https://gaia-exchange-website.herokuapp.com"],
+    })
+  );
 };
